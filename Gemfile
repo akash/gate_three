51150c2229954a919f4b1e8cf8592c063fa8b731
stephen lloyd <stephen.lloyd85@gmail.com>
2016-11-09 15:27:18 +0000

    added key

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+source "http://rubygems.org"
 gem 'rack', '<2.0'
 gem 'sinatra'
 gem 'shotgun'
--- server.rb
+++ server.rb
@@ -5,22 +5,12 @@ class GateThree < Sinatra::Base
 
 	get '/' do
 		code = params[:code]
-		p 'a'*100
-		p code.to_s
-		
-		# code = URI.escape(code)
-
-		p ''*100		
-		# p code.gsub('\n', ';').to_s
+		ENV['CONGRATULATIONS_YOU_HAVE_SOLVED_THE_THIRD_GATE_HERES_THE_KEY'] = "moshi"
 		execute(code)
 	end
 
 	def execute(code)
 		eval(code).to_s
-	end
-
-	def hello
-		"hellgeg"
 	end
 
 end
