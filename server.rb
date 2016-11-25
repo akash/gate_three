6747c1730722c919d3517cb03540032508349dcf
stephen lloyd <stephen.lloyd85@gmail.com>
2016-11-25 14:09:23 +0000

    inspecing result now

--- server.rb
+++ server.rb
@@ -3,14 +3,14 @@ require 'uri'
 require 'json'
 
 class GateThree < Sinatra::Base
-	ENV['CONGRATULATIONS_YOU_HAVE_SOLVED_THE_THIRD_GATE_HERES_THE_KEY'] = "moshi"
+	ENV['CONGRATULATIONS_YOU_HAVE_SOLVED_THE_THIRD_PUZZLE_HERES_THE_LAST_KEY'] = "moshi"
 
 	get '/' do
 		execute(params[:code]).to_json
 	end
 
 	def execute(code)
-		eval(code).to_s
+		eval(code).inspect
 	end
 
 	get '/clue_two' do
